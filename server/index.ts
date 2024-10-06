886e8f9a329cc4bb5f0ff8421d8cee50ea406bd2
Taisei <taisei.960420@gmail.com>
2024-10-06 15:49:33 +0900

    corsの設定、DBから取得したデータを仮置き

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
+
 app.get("/", (req: Request, res: Response) => {
   console.log("getリクエストを受け付けました。");
+  const todos = [
+    { id: "1", todo: "test1"},
+    { id: "2", todo: "test2"},
+    { id: "3", todo: "test3"},
+    { id: "4", todo: "test4"},
+  ]
+
   return res.status(200).json({ message: "hello world" });
 });
 
